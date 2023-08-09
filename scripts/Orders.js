0babfbe6617bb36fd0aefe84e59a9725c3ef80b0
Tricia Swift <tricia.swift8@gmail.com>
2023-08-09 16:27:04 -0500

    wip

--- scripts/Orders.js
+++ scripts/Orders.js
@@ -1,9 +1,18 @@
 export const Orders = async () => {
-  const fetchResponse = await fetch("http://localhost:8088/orders");
+  const fetchResponse = await fetch(
+    "http://localhost:8088/orders?_expand=metal&_expand=style&_expand=size"
+  );
   const orders = await fetchResponse.json();
 
+  // Create a number formatter
+  const formatter = new Intl.NumberFormat("en-US", {
+    style: "currency",
+    currency: "USD",
+  });
+
   let ordersHTML = orders.map((order) => {
-    return `<div>Order #${order.id}</div>`;
+    const orderPrice = order.metal.price + order.style.price + order.size.price;
+    return `<div>Order #${order.id} cost ${formatter.format(orderPrice)}</div>`;
   });
 
   ordersHTML = ordersHTML.join("");
